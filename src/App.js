80cf51f6d93f3178220d9013637c8315d336e98c
Eduard Krigulson <eduard@wisemedia.ee>
2018-04-26 15:49:33 +0300

    Google maps integration

--- src/App.js
+++ src/App.js
@@ -10,12 +10,46 @@ import WelcomeText from './components/MyComponent/WelcomeText';
 import Footer from './components/Footer/Footer';
 import { translate } from 'react-i18next';
 
+import { compose, withProps } from "recompose";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from "react-google-maps";
+
+
 class App extends Component {
   render() {
     const { i18n, t } = this.props;
     const changeLanguage = (lng) => {
       i18n.changeLanguage(lng);
     }
+
+    const MyGoogleMap = compose(
+      withProps({
+        googleMapURL:
+          "https://maps.googleapis.com/maps/api/js?key=AIzaSyAtEIDMy5l_7y8bZAeytovChGugymppVUE&v=3.exp&libraries=geometry,drawing,places",
+        loadingElement: <div style={{ height: `100%` }} />,
+        containerElement: <div style={{
+          height: `200px`,
+          position: 'relative',
+          bottom: 0,
+          top: '500px',
+          width: '800px',
+          left: '25%',
+         }} />,
+        mapElement: <div style={{ height: `100%` }} />
+      }),
+      withScriptjs,
+      withGoogleMap
+    )(props => (
+      <GoogleMap defaultZoom={16} defaultCenter={{ lat: 58.3775219, lng: 26.7310817 }}>
+        {props.isMarkerShown && (
+          <Marker position={{ lat: 58.3775219, lng: 26.7310817 }} />
+        )}
+      </GoogleMap>
+    ));
     return (
       <BrowserRouter>
         <div>
@@ -35,6 +69,16 @@ class App extends Component {
             () => {
               return (
                 <WelcomeText text={t('Hello folks!')} />
+              )
+            }
+          } />
+          <Route path="/where-i-am" exact render={
+            () => {
+              return (
+                <div>
+                  <WelcomeText text={t('Where I am?')} />
+                  <MyGoogleMap isMarkerShown/>
+                </div>
               )
             }
           } />
